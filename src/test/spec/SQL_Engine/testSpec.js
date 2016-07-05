dca8b7563bf0d367fc30cd03cd4db98068431464
Maryna Derevianko <Maryna.Derevianko@travisperkins.co.uk>
2016-07-05 17:59:27 +0300

    Add project structure

---
+++ src/test/spec/SQL_Engine/testSpec.js
@@ -0,0 +1,9 @@
+define(['SQLParser'], function(ParserSql) {
+
+  describe('it should work', function () {
+    it('should do something', function () {
+      expect(true).toBe(true);
+    })
+  });
+
+});
