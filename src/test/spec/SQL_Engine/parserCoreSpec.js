a5d97d0825d6a5e79f125bf0ad38ffa567be8783
Maryna Derevianko <Maryna.Derevianko@travisperkins.co.uk>
2016-07-11 12:56:22 +0300

    Refactoring tests

--- src/test/spec/SQL_Engine/parserCoreSpec.js
+++ src/test/spec/SQL_Engine/parserCoreSpec.js
@@ -16,9 +16,10 @@ define(['SQLParser'], function(SQLParser) {
 
 
       it('should read predefined text', function () {
-
         var test = parser.txt('hello');
-        expect(test.exec('hello', 0)).toEqual({
+        var result = test.exec('hello', 0);
+
+        expect(result).toEqual({
           res: 'hello',
           end: 5
         });
@@ -33,7 +34,9 @@ define(['SQLParser'], function(SQLParser) {
 
       it('should read from specified position', function () {
         var test = parser.txt('hello');
-        expect(test.exec('qqqhello', 3)).toEqual({
+        var result = test.exec('qqqhello', 3);
+
+        expect(result).toEqual({
           res: 'hello',
           end: 8
         });
@@ -49,7 +52,9 @@ define(['SQLParser'], function(SQLParser) {
 
       it('should match predefined regExp', function () {
         var rgx = parser.rgx(/\d+/);
-        expect(rgx.exec('12345qwerty', 0)).toEqual({
+        var result = rgx.exec('12345qwerty', 0);
+
+        expect(result).toEqual({
           res: '12345',
           end: 5
         });
@@ -58,37 +63,45 @@ define(['SQLParser'], function(SQLParser) {
 
       it('should return undefined if regExp does not match', function () {
         var rgx = parser.rgx(/\d+/);
-        expect(rgx.exec('qqqqwerty', 0)).toBeUndefined();
+        var result = rgx.exec('qqqqwerty', 0);
+
+        expect(result).toBeUndefined();
       });
 
 
       it('should match from specified position', function () {
         var rgx = parser.rgx(/\d+/);
-        expect(rgx.exec('qqq12345qwerty', 3)).toEqual({
+        var result = rgx.exec('qqq12345qwerty', 3);
+
+        expect(result).toEqual({
           res: '12345',
           end: 8
         });
       });
     });
 
+
     describe('opt', function() {
       it('should be a function', function () {
         expect(parser.opt).toEqual(jasmine.any(Function));
       });
 
+
       it('should make pattern optional', function () {
         var select = parser.txt('SELECT');
         var optSelect = parser.opt(select);
-        expect(optSelect.exec('SELECT * FROM', 0)).toEqual({
-                                                          res: 'SELECT',
-                                                          end: 6
-                                                        });
+        var goodResult = optSelect.exec('SELECT * FROM', 0);
+        var badResult = optSelect.exec('SLCT * FROM', 0);
 
-        expect(optSelect.exec('SLCT * FROM', 0)).toEqual({
-                                                          res: undefined,
-                                                          end: 0
-                                                        });
+        expect(goodResult).toEqual({
+          res: 'SELECT',
+          end: 6
+        });
 
+        expect(badResult).toEqual({
+          res: undefined,
+          end: 0
+        });
       });
     });
 
@@ -98,25 +111,26 @@ define(['SQLParser'], function(SQLParser) {
         expect(parser.exc).toEqual(jasmine.any(Function));
       });
 
-      it('should parse first pattern', function () {
 
+      it('should parse first pattern', function () {
         var pattern = parser.rgx(/[A-Z]+/);
         var except = parser.txt('FROM');
         var exc = parser.exc(pattern, except);
+        var result = exc.exec('SELECT', 0);
 
-        expect(exc.exec('SELECT', 0)).toEqual({
-                                                res: 'SELECT',
-                                                end: 6
-                                              });
+        expect(result).toEqual({
+          res: 'SELECT',
+          end: 6
+        });
       });
 
       it('should return undefined if it CAN parse second pattern', function () {
-
         var pattern = parser.rgx(/[A-Z]+/);
         var except = parser.txt('FROM');
         var exc = parser.exc(pattern, except);
+        var result = exc.exec('FROM', 0);
 
-        expect(exc.exec('FROM', 0)).toBeUndefined();
+        expect(result).toBeUndefined();
       });
     });
 
@@ -130,17 +144,18 @@ define(['SQLParser'], function(SQLParser) {
         var p1 = parser.txt("abc");
         var p2 = parser.txt("def");
         var any = parser.any(p1, p2);
+        var result1 = any.exec('abc', 0);
+        var result2 = any.exec('def', 0);
 
-        expect(any.exec('abc', 0)).toEqual({
+        expect(result1).toEqual({
           res: 'abc',
           end: 3
         });
 
-        expect(any.exec('def', 0)).toEqual({
+        expect(result2).toEqual({
           res: 'def',
           end: 3
         });
-
       });
 
 
@@ -148,8 +163,9 @@ define(['SQLParser'], function(SQLParser) {
         var p1 = parser.txt("abc");
         var p2 = parser.txt("def");
         var any = parser.any(p1, p2);
+        var result = any.exec('ABC', 0);
 
-        expect(any.exec('ABC', 0)).toBeUndefined();
+        expect(result).toBeUndefined();
       });
     });
 
@@ -159,12 +175,13 @@ define(['SQLParser'], function(SQLParser) {
         expect(parser.seq).toEqual(jasmine.any(Function));
       });
 
-      it('should sequentially parse the text according to the sequence of patterns and return an array of results ', function () {
+      it('should sequentially parse the text according to the sequence of patterns and return an array of results', function () {
         var p1 = parser.txt("abc");
         var p2 = parser.txt("def");
         var seq = parser.seq(p1, p2);
+        var result = seq.exec('abcdef');
 
-        expect(seq.exec('abcdef')).toEqual({
+        expect(result).toEqual({
           res: ["abc", "def"],
           end: 6
         });
@@ -174,32 +191,35 @@ define(['SQLParser'], function(SQLParser) {
         var p1 = parser.txt("abc");
         var p2 = parser.txt("def");
         var seq = parser.seq(p1, p2);
+        var result = seq.exec('abcde1');
 
-        expect(seq.exec('abcde1')).toBeUndefined();
+        expect(result).toBeUndefined();
       });
 
       it('should match from specified position', function () {
         var p1 = parser.txt("abc");
         var p2 = parser.txt("def");
         var seq = parser.seq(p1, p2);
+        var result = seq.exec('qqqabcdef', 3);
 
-        expect(seq.exec('qqqabcdef', 3)).toEqual({
+        expect(result).toEqual({
           res: ["abc", "def"],
           end: 9
         });
       });
     });
 
+
     describe('rep', function() {
       it('should be a function', function () {
         expect(parser.seq).toEqual(jasmine.any(Function));
       });
 
+
       it('should read a repeatable sequence', function () {
         var p = parser.rgx(/\d+/);
         var s = parser.txt(",");
         var rep = parser.rep(p, s);
-
         var result = rep.exec("1,23,456", 0);
 
         expect(result).toEqual({
@@ -208,7 +228,6 @@ define(['SQLParser'], function(SQLParser) {
         });
       });
     });
-
 
   });
 });
